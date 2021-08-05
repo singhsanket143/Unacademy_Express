e78101f77b85291890d047880753ca5a23abbdcf
Sanket Singh <singhsanket143@gmail.com>
2021-08-05 21:16:09 +0530

    Lecture 5: Custom middleware, Babel config, Req params

--- src/server.js
+++ src/server.js
@@ -3,19 +3,63 @@ import morgan from "morgan";
 import { json, urlencoded } from "body-parser";
 
 const app = express();
+const router = express.Router();
+
 
 app.use(morgan('dev'));
+app.use(json());
+app.use(urlencoded({extended: true}));
 
+const customLogger = (req, res, next) => {
+    console.log("Logger incoming");
+    console.log(req.body);
+    next();
+}
+
+app.use('/api/v1',router);
 
 app.get('/', (req, res) => {
-    console.log(req.body);
-    res.send({message: "OK"});
+    res.send({message: "OK GET"});
 });
 
-app.post('/', (req, res) => {
+app.post('/', customLogger, (req, res) => {
     console.log(req.body);
     res.send({message: "OK POST"});
 });
+
+
+// router.get('/post', (req, res) => {
+//     res.send({message: "Router OK"});
+// });
+
+// router.post('/post', (req, res) => {
+//     res.send({message: "OK"});
+// });
+
+
+router
+    .route('/post')
+    .get((req, res) => {
+        res.send({message: "Router OK GET"});
+    })
+    .post((req, res) => {
+        res.send({message: "Router OK POST"});
+    });
+
+router 
+    .route('/post/:id/:num')
+    .put((req, res) => {
+        console.log(req.params);
+        res.send({message: "Router OK PUT"});
+    })
+    .patch((req, res) => {
+        res.send({message: "Router OK PATCH"});
+    })
+    .delete((req, res) => {
+        res.send({message: "Router OK DELETE"});
+    });
+
+
 
 export const start = () => {
     app.listen(3000, () => {
